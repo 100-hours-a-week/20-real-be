98d3995c3ed186b3e405dd649cb40badba936e5a
tpgh02 <seaho4588@gmail.com>
2025-04-29 08:30:24 +0900

    init Data Response

---
+++ src/main/java/com/real/backend/response/DataResponse.java
@@ -0,0 +1,17 @@
+package com.real.backend.response;
+
+import lombok.Getter;
+
+@Getter
+public class DataResponse<T> extends ResponseDTO {
+    private T data;
+
+    private DataResponse(T data) {
+        super(StatusCode.OK.getStatus(), StatusCode.OK.getMessage());
+        this.data = data;
+    }
+
+    public static <T> DataResponse<T> of(T data) {
+        return new DataResponse<>(data);
+    }
+}
