ac7480a77b09f652aaeba108e47786686fbf8f7e
Changsu Park <gd12091@gmail.com>
2025-05-01 14:23:12 +0900

    :sparkles: feat: HealthCheck API 개발

--- src/main/java/com/real/backend/config/SecurityConfig.java
+++ src/main/java/com/real/backend/config/SecurityConfig.java
@@ -56,6 +56,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests((auth) -> auth
                         .requestMatchers("/auth/**").permitAll() // 필터 거치지 않고 통과
                         .requestMatchers("/api/v1/oauth/**").permitAll()
+                        .requestMatchers("/api/healthz").permitAll()    //서버 헬스 체크용 API, Security Filter 우회
                         .anyRequest().authenticated()  // 나머지는 필터 통과
                 );
 
