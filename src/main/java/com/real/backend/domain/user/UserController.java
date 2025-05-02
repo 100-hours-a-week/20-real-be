8f23317f46630c093acf47ecab2b19ce9110cac8
tpgh02 <seaho4588@gmail.com>
2025-05-02 15:10:05 +0900

    ♻️ refactor: 카카오 로그인 성공 로직 변경
    카카오 로그인 성공시 /login/success로 리다이렉트 후 데이터 전달 api 호출

--- src/main/java/com/real/backend/domain/user/UserController.java
+++ src/main/java/com/real/backend/domain/user/UserController.java
@@ -1,7 +1,15 @@
 package com.real.backend.domain.user;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.real.backend.domain.user.domain.User;
+import com.real.backend.domain.user.dto.LoginResponseDTO;
+import com.real.backend.domain.user.service.UserService;
+import com.real.backend.response.DataResponse;
+import com.real.backend.security.CurrentSession;
+import com.real.backend.security.Session;
 
 import lombok.RequiredArgsConstructor;
 
@@ -9,4 +17,13 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @RequestMapping("/api")
 public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/v1/users/info")
+    public DataResponse<?> getUserInfo(@CurrentSession Session session) {
+        LoginResponseDTO loginResponseDTO = userService.getUserInfo(session.getId());
+
+        return DataResponse.of(loginResponseDTO);
+    }
 }
