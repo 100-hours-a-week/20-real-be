fbc44791af240e37f0e16565a02da522e604dd57
tpgh02 <seaho4588@gmail.com>
2025-05-02 17:08:27 +0900

    🚑 !HOTFIX: 카카오 연동 수정

--- src/main/java/com/real/backend/domain/user/dto/LoginResponseDTO.java
+++ src/main/java/com/real/backend/domain/user/dto/LoginResponseDTO.java
@@ -1,6 +1,7 @@
 package com.real.backend.domain.user.dto;
 
 import com.real.backend.domain.user.domain.Role;
+import com.real.backend.domain.user.domain.User;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -13,4 +14,12 @@ public class LoginResponseDTO {
     private String nickname;
     private Role role;
     private String profileUrl;
+
+    public static LoginResponseDTO from(User user) {
+        return LoginResponseDTO.builder()
+            .nickname(user.getNickname())
+            .role(user.getRole())
+            .profileUrl(user.getProfileUrl())
+            .build();
+    }
 }
