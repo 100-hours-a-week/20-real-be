18cdb5700d52e98c0f59a434ce2af5d6150484a8
tpgh02 <seaho4588@gmail.com>
2025-05-04 13:45:28 +0900

    ♻️ refactor: 뉴스 목록 반환 dto에 태그 추가

--- src/main/java/com/real/backend/domain/news/dto/NewsListResponseDTO.java
+++ src/main/java/com/real/backend/domain/news/dto/NewsListResponseDTO.java
@@ -7,22 +7,25 @@ import com.real.backend.domain.news.domain.News;
 import lombok.Builder;
 
 @Builder
-public record NewsListResponseDTO(
-    Long id,
-    String title,
-    Long commentCount,
-    Long todayViewCount,
-    String imageUrl,
+public class NewsListResponseDTO{
+    private Long id;
+    private String title;
+    private String tag;
+    private Long commentCount;
+    private Long todayViewCount;
+    private String imageUrl;
     @JsonFormat(pattern = "yyyy.MM.dd HH:mm:ss")
-    LocalDateTime createdAt) {
+    private LocalDateTime createdAt;
+
     public static NewsListResponseDTO of(News news) {
         return NewsListResponseDTO.builder()
-                    .id(news.getId())
-                    .title(news.getTitle())
-                    .commentCount(news.getCommentCount())
-                    .todayViewCount(news.getTodayViewCount())
-                    .imageUrl(news.getImageUrl())
-                    .createdAt(news.getCreatedAt())
-                    .build();
+            .id(news.getId())
+            .title(news.getTitle())
+            .tag(news.getTag())
+            .commentCount(news.getCommentCount())
+            .todayViewCount(news.getTodayViewCount())
+            .imageUrl(news.getImageUrl())
+            .createdAt(news.getCreatedAt())
+            .build();
     }
 }
