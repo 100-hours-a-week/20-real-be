648a685f53a71ca838c0d4aa23f0b01078516245
tpgh02 <seaho4588@gmail.com>
2025-05-07 13:36:30 +0900

    feat: 공지 생성하는 임시 api 생성

---
+++ src/main/java/com/real/backend/domain/notice/tmp/NoticeTmpService.java
@@ -0,0 +1,80 @@
+package com.real.backend.domain.notice.tmp;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.real.backend.domain.notice.domain.Notice;
+import com.real.backend.domain.notice.domain.NoticeFile;
+import com.real.backend.domain.notice.repository.NoticeFileRepository;
+import com.real.backend.domain.notice.repository.NoticeRepository;
+import com.real.backend.domain.user.domain.User;
+import com.real.backend.domain.user.repository.UserRepository;
+import com.real.backend.util.S3Utils;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class NoticeTmpService {
+
+    private final NoticeRepository noticeRepository;
+    private final UserRepository userRepository;
+    private final S3Utils s3Utils;
+    private final NoticeFileRepository noticeFileRepository;
+
+    @Transactional
+    public void createNoticeTmp(NoticeCreateRequestTmpDTO noticeCreateRequestDTO, List<MultipartFile> images,
+        List<MultipartFile> files) {
+
+        String userName = noticeCreateRequestDTO.userName();
+        User user = userRepository.findByNickname(userName);
+
+        Notice notice = noticeRepository.save(Notice.builder()
+            .user(user)
+            .title(noticeCreateRequestDTO.title())
+            .content(noticeCreateRequestDTO.content())
+            //.summary()
+            .platform(noticeCreateRequestDTO.platform())
+            .tag(noticeCreateRequestDTO.tag())
+            .originalUrl(noticeCreateRequestDTO.originalUrl())
+            .totalViewCount(0L)
+            .commentCount(0L)
+            .likeCount(0L)
+            .build());
+
+        int i = 0;
+        for (MultipartFile file:files){
+            String url = s3Utils.upload(file, "notice/files");
+            String name = file.getOriginalFilename();
+            assert name != null;
+            String type = name.substring(name.lastIndexOf(".") + 1);
+            Integer fileSeqNo = i;
+            noticeFileRepository.save(NoticeFile.builder()
+                .notice(notice)
+                .fileSeqNo(fileSeqNo)
+                .fileUrl(url)
+                .type(type)
+                .build());
+            i++;
+        }
+        i = 0;
+        for (MultipartFile file:images){
+            String url =s3Utils.upload(file, "notice/images");
+            String name = file.getOriginalFilename();
+            assert name != null;
+            String type = name.substring(name.lastIndexOf(".") + 1);
+            Integer fileSeqNo = i;
+
+            noticeFileRepository.save(NoticeFile.builder()
+                .notice(notice)
+                .fileSeqNo(fileSeqNo)
+                .fileUrl(url)
+                .type(type)
+                .build());
+            i++;
+        }
+    }
+}
--- src/main/java/com/real/backend/security/filter/JwtFilter.java
+++ src/main/java/com/real/backend/security/filter/JwtFilter.java
@@ -83,7 +83,7 @@ public class JwtFilter extends OncePerRequestFilter {
     }
 
     private boolean shouldSkip(HttpServletRequest request) {
-        List<String> skipURI = Arrays.asList("/login", "/auth/.*", "/users/signup", "/api/v1/oauth/.*", "/api/v1/news", "/api/healthz", "/error");
+        List<String> skipURI = Arrays.asList("/login", "/auth/.*", "/users/signup", "/api/v1/oauth/.*", "/api/v1/news", "/api/healthz", "/error", "/api/v1/notices/tmp");
         return skipURI.stream().anyMatch(uri -> {
             Pattern pattern = Pattern.compile(uri);
             return pattern.matcher(request.getRequestURI()).matches();
