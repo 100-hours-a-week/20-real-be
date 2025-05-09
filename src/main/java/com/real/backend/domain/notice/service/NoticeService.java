f861117fbbdded026b4529bf2d17c6d69e088101
tpgh02 <seaho4588@gmail.com>
2025-05-09 17:18:40 +0900

    ♻️️ refactor: 공지 생성시 요약 추가

--- src/main/java/com/real/backend/domain/notice/service/NoticeService.java
+++ src/main/java/com/real/backend/domain/notice/service/NoticeService.java
@@ -29,6 +29,7 @@ import com.real.backend.domain.user.component.UserFinder;
 import com.real.backend.domain.user.domain.User;
 import com.real.backend.domain.user.repository.UserRepository;
 import com.real.backend.exception.NotFoundException;
+import com.real.backend.exception.ServerException;
 import com.real.backend.infra.ai.dto.NoticeSummaryRequestDTO;
 import com.real.backend.infra.ai.dto.NoticeSummaryResponseDTO;
 import com.real.backend.infra.ai.service.NoticeAiService;
@@ -158,17 +159,17 @@ public class NoticeService {
         User user = userRepository.findByNickname(userName);
 
         // ai에 summary 요청 로직
-        // NoticeSummaryRequestDTO noticeSummaryRequestDTO = new NoticeSummaryRequestDTO(noticeCreateRequestDTO.content(),
-        //     noticeCreateRequestDTO.title());
-        // NoticeSummaryResponseDTO noticeSummaryResponseDTO = null;
-        // for (int i = 0; i < 3; i++) {
-        //     noticeSummaryResponseDTO = noticeAiService.makeSummary(noticeSummaryRequestDTO);
-        //     if (noticeSummaryResponseDTO.isCompleted())
-        //         break;
-        // }
-        // if (!noticeSummaryResponseDTO.isCompleted()){
-        //     throw new ServerException("ai가 응답을 주지 못했습니다.");
-        // }
+        NoticeSummaryRequestDTO noticeSummaryRequestDTO = new NoticeSummaryRequestDTO(noticeCreateRequestDTO.content(),
+            noticeCreateRequestDTO.title());
+        NoticeSummaryResponseDTO noticeSummaryResponseDTO = null;
+        for (int i = 0; i < 3; i++) {
+            noticeSummaryResponseDTO = noticeAiService.makeSummary(noticeSummaryRequestDTO);
+            if (noticeSummaryResponseDTO.isCompleted())
+                break;
+        }
+        if (!noticeSummaryResponseDTO.isCompleted()){
+            throw new ServerException("ai가 응답을 주지 못했습니다.");
+        }
 
         LocalDateTime createTime = LocalDateTime.parse(noticeCreateRequestDTO.createdAt());
 
@@ -176,7 +177,7 @@ public class NoticeService {
             .user(user)
             .title(noticeCreateRequestDTO.title())
             .content(noticeCreateRequestDTO.content())
-            // .summary(noticeSummaryResponseDTO.summary())
+            .summary(noticeSummaryResponseDTO.summary())
             .platform(noticeCreateRequestDTO.platform())
             .tag(noticeCreateRequestDTO.tag())
             .originalUrl(noticeCreateRequestDTO.originalUrl())
@@ -186,45 +187,7 @@ public class NoticeService {
             .createdAt(createTime)
             .build());
 
-        int i = 0;
-        if (images == null)
-            images = new ArrayList<>();
-        if (files == null)
-            files = new ArrayList<>();
-        for (MultipartFile file : files) {
-            String url = s3Utils.upload(file, "static/notice/files");
-            String name = file.getOriginalFilename();
-            String type = "";
-            if (name != null && name.contains(".")) {
-                type = name.substring(name.lastIndexOf(".") + 1).toLowerCase();
-            }
-            i++;
-            Integer fileSeqNo = i;
-            noticeFileRepository.save(NoticeFile.builder()
-                .notice(notice)
-                .fileSeqNo(fileSeqNo)
-                .fileUrl(url)
-                .type(type)
-                .build());
-
-        }
-        i = 0;
-        for (MultipartFile file : images) {
-            String url = s3Utils.upload(file, "static/notice/images");
-            String name = file.getOriginalFilename();
-            String type = "";
-            if (name != null && name.contains(".")) {
-                type = name.substring(name.lastIndexOf(".") + 1).toLowerCase();
-            }
-            Integer fileSeqNo = i;
-            i++;
-            noticeFileRepository.save(NoticeFile.builder()
-                .notice(notice)
-                .fileSeqNo(fileSeqNo)
-                .fileUrl(url)
-                .type(type)
-                .build());
-
-        }
+        noticeFileService.uploadFilesToS3(images, notice, true);
+        noticeFileService.uploadFilesToS3(files, notice, false);
     }
 }
