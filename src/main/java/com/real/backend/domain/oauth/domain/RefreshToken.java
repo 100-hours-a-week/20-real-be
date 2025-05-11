c119588a514faf17c355523a7082f7d3b2226a1d
tpgh02 <seaho4588@gmail.com>
2025-05-11 21:46:04 +0900

    ✨feat : refresh token 저장 로직 구현

---
+++ src/main/java/com/real/backend/domain/oauth/domain/RefreshToken.java
@@ -0,0 +1,38 @@
+package com.real.backend.domain.oauth.domain;
+
+import java.time.LocalDateTime;
+
+import org.checkerframework.checker.units.qual.C;
+
+import com.real.backend.domain.user.domain.User;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class RefreshToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String token;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    private LocalDateTime expiryTime;
+}
