682b52aad190bd95d741cf0c7eac6f9de8566073
tpgh02 <seaho4588@gmail.com>
2025-06-13 16:01:20 +0900

    ✨ feat: refresh token을 redis로 관리

--- src/main/java/com/real/backend/modules/auth/service/RefreshTokenService.java
+++ src/main/java/com/real/backend/modules/auth/service/RefreshTokenService.java
@@ -3,13 +3,12 @@ package com.real.backend.modules.auth.service;
 import java.time.Duration;
 import java.time.LocalDateTime;
 
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.real.backend.common.exception.UnauthorizedException;
 import com.real.backend.common.util.CONSTANT;
-import com.real.backend.modules.auth.domain.RefreshToken;
-import com.real.backend.modules.auth.repository.RefreshTokenRepository;
 import com.real.backend.modules.user.domain.User;
 import com.real.backend.security.JwtUtils;
 
@@ -18,20 +17,18 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class RefreshTokenService {
-    private final RefreshTokenRepository refreshTokenRepository;
     private final JwtUtils jwtUtils;
+
+    private final RedisTemplate<String, String> redisTemplate;
 
     @Transactional
     public String generateRefreshToken(User user) {
         String refreshToken = jwtUtils.generateToken("refresh", user.getId(), user.getNickname(), user.getRole().toString(),
             CONSTANT.REFRESH_TOKEN_EXPIRED);
-        LocalDateTime expiryTime = LocalDateTime.now().plus(Duration.ofSeconds(CONSTANT.REFRESH_TOKEN_EXPIRED));
-        refreshTokenRepository.deleteByUser(user);
-        refreshTokenRepository.save(RefreshToken.builder()
-            .token(refreshToken)
-            .user(user)
-            .expiryTime(expiryTime)
-            .build());
+
+        String refreshKey = "refresh:" + refreshToken;
+
+        redisTemplate.opsForValue().set(refreshKey, user.getId().toString(), Duration.ofSeconds(CONSTANT.REFRESH_TOKEN_EXPIRED));
         return refreshToken;
     }
 
@@ -40,6 +37,6 @@ public class RefreshTokenService {
         if (refreshToken == null) {
             throw new UnauthorizedException("리프레시 토큰이 없습니다.");
         }
-        refreshTokenRepository.deleteByToken(refreshToken);
+        redisTemplate.delete("refresh:" + refreshToken);
     }
 }
--- src/main/java/com/real/backend/modules/auth/service/TokenService.java
+++ src/main/java/com/real/backend/modules/auth/service/TokenService.java
@@ -1,12 +1,11 @@
 package com.real.backend.modules.auth.service;
 
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.real.backend.common.exception.UnauthorizedException;
-import com.real.backend.modules.auth.domain.RefreshToken;
 import com.real.backend.modules.auth.dto.TokenDTO;
-import com.real.backend.modules.auth.repository.RefreshTokenRepository;
 import com.real.backend.modules.user.component.UserFinder;
 import com.real.backend.modules.user.domain.User;
 import com.real.backend.security.JwtUtils;
@@ -17,21 +16,23 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class TokenService {
     private final RefreshTokenService refreshTokenService;
-    private final RefreshTokenRepository refreshTokenRepository;
     private final AccessTokenService accessTokenService;
     private final JwtUtils jwtUtils;
     private final UserFinder userFinder;
+    private final RedisTemplate<String, Object> redisTemplate;
 
     @Transactional
     public TokenDTO refreshAccessToken(String refreshToken) {
+        String refreshKey = "refresh:" + refreshToken;
         if (refreshToken == null) { throw new UnauthorizedException("refresh token is null"); }
 
         User user = userFinder.getUser(jwtUtils.getId(refreshToken));
-        RefreshToken saved = refreshTokenRepository.findByUser(user).orElseThrow(() -> new UnauthorizedException("refresh token not found"));
 
-        if (!saved.getToken().equals(refreshToken)) {
+        if (redisTemplate.opsForValue().get(refreshKey) == null) {
             throw new UnauthorizedException("refresh token does not match");
         }
+
+        refreshTokenService.deleteRefreshToken(refreshToken);
 
         String accessToken = accessTokenService.generateAccessToken(user);
         String newRefreshToken = refreshTokenService.generateRefreshToken(user);
