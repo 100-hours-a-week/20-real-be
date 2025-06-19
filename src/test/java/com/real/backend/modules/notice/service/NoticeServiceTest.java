fcbe8c0fdf0ec602e6998efb43a8b346608acd97
tpgh02 <seaho4588@gmail.com>
2025-06-19 13:45:08 +0900

    ✅ test: createNotice 테스트 작성

--- src/test/java/com/real/backend/modules/notice/service/NoticeServiceTest.java
+++ src/test/java/com/real/backend/modules/notice/service/NoticeServiceTest.java
@@ -1,6 +1,7 @@
 package com.real.backend.modules.notice.service;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.mockito.BDDMockito.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -11,12 +12,17 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.real.backend.common.config.RedisConfig;
 import com.real.backend.common.util.dto.SliceDTO;
+import com.real.backend.infra.ai.dto.NoticeSummaryRequestDTO;
+import com.real.backend.infra.ai.dto.NoticeSummaryResponseDTO;
 import com.real.backend.infra.redis.NoticeRedisService;
 import com.real.backend.modules.notice.domain.Notice;
+import com.real.backend.modules.notice.dto.NoticeCreateRequestDTO;
 import com.real.backend.modules.notice.dto.NoticeListResponseDTO;
 import com.real.backend.modules.notice.repository.NoticeRepository;
 import com.real.backend.modules.user.domain.LoginType;
@@ -40,6 +46,9 @@ class NoticeServiceTest extends NoticeServiceTestIntegration {
 
     @Autowired
     private NoticeRedisService noticeRedisService;
+
+    @MockitoBean
+    private NoticeAiService noticeAiService;
 
     @DisplayName("getNoticeListByCursor 성공: 읽음 여부 확인 및 공지의 첫 페이지를 최대 10개까지 보여준다.")
     @Test
@@ -107,6 +116,42 @@ class NoticeServiceTest extends NoticeServiceTestIntegration {
             .mapToObj(i -> notices.get(i).getId())
             .toList();
         assertThat(resultIds).doesNotContainAnyElementsOf(previousIds);
+    }
+
+    @DisplayName("createNotice 성공: 공지가 정상적으로 생성")
+    @Test
+    void createNotice_success() throws JsonProcessingException {
+        // given
+        User user = createUser();
+        Long userId = user.getId();
+        NoticeCreateRequestDTO dto = NoticeCreateRequestDTO.builder()
+            .title("제목")
+            .content("내용")
+            .tag("공지")
+            .originalUrl("https://example.com/original")
+            .build();
+        NoticeSummaryRequestDTO noticeSummaryRequestDTO = new NoticeSummaryRequestDTO(dto.getContent(), dto.getTitle());
+        NoticeSummaryResponseDTO noticeSummaryResponseDTO = new NoticeSummaryResponseDTO("요약", true);
+        given(noticeAiService.makeSummary(noticeSummaryRequestDTO)).willReturn(noticeSummaryResponseDTO);
+
+        // when
+        noticeService.createNotice(userId, dto);
+
+        // then
+        List<Notice> result = noticeRepository.findAll();
+        assertThat(result).hasSize(1);
+
+        Notice notice = result.get(0);
+        assertThat(notice.getTitle()).isEqualTo("제목");
+        assertThat(notice.getContent()).isEqualTo("내용");
+        assertThat(notice.getUser().getId()).isEqualTo(userId);
+        assertThat(notice.getTag()).isEqualTo("공지");
+        assertThat(notice.getOriginalUrl()).isEqualTo("https://example.com/original");
+        assertThat(notice.getSummary()).isEqualTo("요약");
+        assertThat(notice.getDeletedAt()).isNull();
+        assertThat(notice.getTotalViewCount()).isEqualTo(0);
+        assertThat(notice.getCommentCount()).isEqualTo(0);
+        assertThat(notice.getLikeCount()).isEqualTo(0);
     }
 
     private User createUser() {
