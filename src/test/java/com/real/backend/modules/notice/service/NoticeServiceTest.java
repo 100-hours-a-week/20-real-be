7d141c1926ca81443f750469cf626b9311b25ce8
tpgh02 <seaho4588@gmail.com>
2025-06-20 16:32:44 +0900

    ✅ test: 공지 조회, 삭제 테스트 코드 작성

--- src/test/java/com/real/backend/modules/notice/service/NoticeServiceTest.java
+++ src/test/java/com/real/backend/modules/notice/service/NoticeServiceTest.java
@@ -15,11 +15,14 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.real.backend.common.config.RedisConfig;
+import com.real.backend.common.exception.NotFoundException;
 import com.real.backend.common.util.dto.SliceDTO;
 import com.real.backend.config.AiResponseConfig;
 import com.real.backend.infra.redis.NoticeRedisService;
+import com.real.backend.infra.redis.PostRedisService;
 import com.real.backend.modules.notice.domain.Notice;
 import com.real.backend.modules.notice.dto.NoticeCreateRequestDTO;
+import com.real.backend.modules.notice.dto.NoticeInfoResponseDTO;
 import com.real.backend.modules.notice.dto.NoticeListResponseDTO;
 import com.real.backend.modules.notice.repository.NoticeRepository;
 import com.real.backend.modules.user.domain.LoginType;
@@ -43,6 +46,9 @@ class NoticeServiceTest extends NoticeServiceTestIntegration {
 
     @Autowired
     private NoticeRedisService noticeRedisService;
+
+    @Autowired
+    private PostRedisService postRedisService;
 
     @DisplayName("getNoticeListByCursor 성공: 읽음 여부 확인 및 공지의 첫 페이지를 최대 10개까지 보여준다.")
     @Test
@@ -144,6 +150,111 @@ class NoticeServiceTest extends NoticeServiceTestIntegration {
         assertThat(notice.getCommentCount()).isEqualTo(0);
         assertThat(notice.getLikeCount()).isEqualTo(0);
     }
+
+    @DisplayName("createNotice 실패: 유효하지 않은 사용자 id로 인해 공지를 작성할 사용자가 존재하지 않아서 실패")
+    @Test
+    void createNotice_fail() {
+        // given
+        Long userId = 999999L;
+        NoticeCreateRequestDTO dto = NoticeCreateRequestDTO.builder()
+            .title("제목")
+            .content("내용")
+            .tag("공지")
+            .originalUrl("https://example.com/original")
+            .build();
+
+        // when & then
+        assertThatThrownBy(() -> noticeService.createNotice(userId, dto))
+            .isInstanceOf(NotFoundException.class)
+            .hasMessageContaining("해당 id를 가진 사용자가 없습니다.");
+    }
+
+    @DisplayName("getNoticeById 성공: 공지 하나의 정보를 성공적으로 불러옴")
+    @Test
+    void getNoticeById_success() {
+        // given
+        User user = createUser();
+        Long userId = user.getId();
+        Notice notice = createNotices(user, 3).getFirst();
+        Long noticeId = notice.getId();
+
+        postRedisService.initCount("notice", "totalView", noticeId, 0L);
+        postRedisService.initCount("notice", "like", noticeId, 2L);
+        postRedisService.initCount("notice", "comment", noticeId, 5L);
+        postRedisService.toggleLikeInRedis("notice", userId, noticeId);
+
+        // when
+        NoticeInfoResponseDTO result = noticeService.getNoticeById(noticeId, userId);
+
+        // then
+        assertThat(result.getId()).isEqualTo(noticeId);
+        assertThat(result.getTitle()).isEqualTo(notice.getTitle());
+        assertThat(result.getContent()).isEqualTo(notice.getContent());
+        assertThat(result.getLikeCount()).isEqualTo(3L);
+        assertThat(result.getCommentCount()).isEqualTo(5L);
+        assertThat(result.getUserLike()).isTrue();
+        assertThat(result.getFiles()).isEmpty();
+        assertThat(result.getImages()).isEmpty();
+    }
+
+    @DisplayName("getNoticeById 실패: 삭제된 공지를 불러오려고 해서 실패")
+    @Test
+    void getNoticeById_fail_deletedNotice() {
+        // given
+        User user = createUser();
+        Long userId = user.getId();
+        Notice notice = createNotices(null, 3).getFirst();
+        Long noticeId = notice.getId();
+        notice.delete();
+
+        // when & then
+        assertThatThrownBy(() -> noticeService.getNoticeById(noticeId, userId))
+            .isInstanceOf(NotFoundException.class)
+            .hasMessageContaining("해당 id를 가진 공지가 존재하지 않습니다.");
+    }
+
+    @DisplayName("getNoticeById 실패: 공지가 존재하지 않음 ")
+    @Test
+    void getNoticeById_fail_noticeNotFound() {
+        // given
+        User user = createUser();
+        Long userId = user.getId();
+        Long noticeId = 1L;
+
+        // when & then
+        assertThatThrownBy(() -> noticeService.getNoticeById(noticeId, userId))
+            .isInstanceOf(NotFoundException.class)
+            .hasMessageContaining("해당 id를 가진 공지가 존재하지 않습니다.");
+    }
+
+    @DisplayName("deleteNotice 성공: 공지 삭제 요청이 오면 해당 공지의 deletedAt을 now()로 변경한다.")
+    @Test
+    void deleteNotice_success() {
+        // given
+        Notice notice = createNotices(createUser(), 1).getFirst();
+        Long noticeId = notice.getId();
+
+        // when
+        noticeService.deleteNotice(noticeId);
+
+        // then
+        Notice result = noticeRepository.findById(noticeId).orElse(null);
+        assertThat(result).isNotNull();
+        assertThat(result.getDeletedAt()).isNotNull();
+    }
+
+    @DisplayName("deleteNotice 실패: 공지가 존재하지 않음.")
+    @Test
+    void deleteNotice_fail_noticeNotFound() {
+        // given
+        Long noticeId = 1L;
+
+        // when & then
+        assertThatThrownBy(() -> noticeService.deleteNotice(noticeId))
+            .isInstanceOf(NotFoundException.class)
+            .hasMessageContaining("해당 id를 가진 공지가 존재하지 않습니다.");
+    }
+
 
     private User createUser() {
         User user = User.builder()
