0c20d020bcdff2a7a2d2a16f248e2048f81e99d4
tpgh02 <seaho4588@gmail.com>
2025-07-01 01:06:54 +0900

    ✨ feat: access token 유효 시간 변경
    60분 -> 5분

--- src/main/java/com/real/backend/common/util/CONSTANT.java
+++ src/main/java/com/real/backend/common/util/CONSTANT.java
@@ -5,7 +5,7 @@ public class CONSTANT {
     private CONSTANT() {}
 
     // JWT Expire time
-    public static final long ACCESS_TOKEN_EXPIRED = 60 * 60L; // 1시간
+    public static final long ACCESS_TOKEN_EXPIRED = 5 * 60L; // 5분
     public static final long REFRESH_TOKEN_EXPIRED = 14 * 24 * 60 * 60L; // 14일
 
 }
