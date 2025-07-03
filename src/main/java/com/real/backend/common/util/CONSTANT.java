841cfcec361b0994039417457707bcb4ec226668
tpgh02 <seaho4588@gmail.com>
2025-07-03 14:40:17 +0900

    ✨ feat: 연결 시간, 하트비트 시간 설정

--- src/main/java/com/real/backend/common/util/CONSTANT.java
+++ src/main/java/com/real/backend/common/util/CONSTANT.java
@@ -8,4 +8,8 @@ public class CONSTANT {
     public static final long ACCESS_TOKEN_EXPIRED = 5 * 60L; // 5분
     public static final long REFRESH_TOKEN_EXPIRED = 14 * 24 * 60 * 60L; // 14일
 
+    // SSE connection time
+    public static final long CONNECTION_TIMEOUT = 5 * 60 * 1000L; // 5분
+    public static final long HEARTBEAT_INTERVAL = 10 * 1000L; // 10초
+
 }
